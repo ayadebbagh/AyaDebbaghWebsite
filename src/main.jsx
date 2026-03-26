e2d03b32733a3218a13da1914acaf35f454f0581
Aya Debbagh <debbagh.aya@gmail.com>
2026-03-26 13:25:13 -0400

    Feature: add vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Analytics } from '@vercel/analytics/react';
 import './index.css';
 import App from './App.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <>
+    <App />
+    <Analytics />
+  </>
+);
